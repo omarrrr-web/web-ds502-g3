f8c0630be50956d798260cdac9fdee16a685814a
omarrrr-web <oenriquezseas@gmail.com>
2025-11-06 08:08:26 -0500

    Clean up RolController by removing comment
    Removed unnecessary comment from RolController.

--- api/controllers/RolController.php
+++ api/controllers/RolController.php
@@ -6,6 +6,4 @@ class RolController extends BaseController {
     public function __construct($pdo) {
         parent::__construct($pdo, 'roles', 'id_rol');
     }
-
-    // No se necesita anadir nada mas, ya que Rol usa el CRUD generico.
 }
